5d031ae5b45bd0d2f6aa76dbb230c731462e9bb5
Sébastien Perrot <sebastien.perrot@saegus.com>
2019-07-08 11:00:31 +0200

    Add animation on timeline

--- src/app/components/organisms/timeline/timeline.component.ts
+++ src/app/components/organisms/timeline/timeline.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-timeline',
@@ -7,10 +8,30 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class TimelineComponent implements OnInit {
   @Input() elements:any;
+  element = document.getElementsByClassName('saegus-timeline');
+  blocks = document.getElementsByClassName("cd-timeline__block");
   
   constructor() { }
 
-  ngOnInit() {
+  bouncing = () => {
+    let self = this;
+    for (var i = 0; i < this.blocks.length; i++) {
+      (function (i) {
+        if (self.blocks[i].classList.contains("cd-timeline--hidden") && self.blocks[i].getBoundingClientRect().top <= window.innerHeight) {
+          self.blocks[i].classList.add("cd-timeline--bounce-in");
+          self.blocks[i].classList.remove("cd-timeline--hidden");
+        }
+        if (document.getElementsByClassName('cd-timeline--bounce-in').length == self.blocks.length) window.removeEventListener('scroll', self.bouncing, true);
+      })(i);
+    };
   }
 
+  ngAfterViewInit() {
+    window.addEventListener('scroll', this.bouncing, true);
+  }
+  ngOnDestroy() {
+    window.removeEventListener('scroll', this.bouncing, true);
+  }
+
+  ngOnInit() {}
 }
